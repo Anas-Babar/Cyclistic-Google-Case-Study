dd5eafb5486d7dbf3b789e2b1d4e948c22d6f5f1
Anas-Babar <125931322+Anas-Babar@users.noreply.github.com>
2024-04-15 02:49:56 +0500

    Update data_exploration.sql

--- data_exploration.sql
+++ data_exploration.sql
@@ -1,6 +1,1 @@
-SELECT*
 
-FROM `bike-share-2024.Bikeshare2023.Year - 2023`
-
-WHERE
-ride_id is not null AND rideable_type is not null AND started_at is not null AND ended_at is not null AND start_station_name is not null AND start_station_id is not null AND end_station_name is not null AND end_station_id is not null AND end_station_name is not null AND end_station_id is not null AND start_lat is not null AND start_lng is not null AND end_lat is not null AND end_lng is not null AND member_casual is not NULL
